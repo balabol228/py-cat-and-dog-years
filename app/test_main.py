6be64f3bba7b87c302771e8609c826265bac1a89
Denys Baliuk <denuk14228@gmail.com>
2026-03-23 22:49:38 +0200

    cat and dog years 4

--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,7 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age, expected",
+    ("cat_age", "dog_age", "expected"),
     [
         (0, 0, [0, 0]),
         (1, 1, [0, 0]),
@@ -19,18 +19,23 @@ from app.main import get_human_age
         (100, 100, [21, 17]),
     ],
 )
-def test_get_human_age(cat_age, dog_age, expected):
-    assert get_human_age(cat_age, dog_age) == expected
+def test_get_human_age(
+    cat_age: int,
+    dog_age: int,
+    expected: list[int],
+) -> None:
+    result: list[int] = get_human_age(cat_age, dog_age)
+    assert result == expected
 
 
-def test_cat_edge_cases():
+def test_cat_edge_cases() -> None:
     assert get_human_age(14, 0)[0] == 0
     assert get_human_age(15, 0)[0] == 1
     assert get_human_age(24, 0)[0] == 2
     assert get_human_age(28, 0)[0] == 3
 
 
-def test_dog_edge_cases():
+def test_dog_edge_cases() -> None:
     assert get_human_age(0, 14)[1] == 0
     assert get_human_age(0, 15)[1] == 1
     assert get_human_age(0, 24)[1] == 2
