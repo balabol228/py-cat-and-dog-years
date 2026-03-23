913e616a04e670f4f2770dbfb413abd45ef88020
Denys Baliuk <denuk14228@gmail.com>
2026-03-23 22:56:28 +0200

    cat and dog years 7

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 def get_human_age(cat_age: int, dog_age: int) -> list[int]:
-    def cat_to_human(age: int) -> int:
+    def cat(age: int) -> int:
         if age < 15:
             return 0
         if age < 24:
             return 1
-
         return 2 + (age - 24) // 4
 
-    def dog_to_human(age: int) -> int:
+    def dog(age: int) -> int:
         if age < 15:
             return 0
         if age < 24:
             return 1
+        if age < 29:
+            return 2
+        return 3 + (age - 29) // 5
 
-        return 2 + (age - 24) // 5
-
-    return [cat_to_human(cat_age), dog_to_human(dog_age)]
+    return [cat(cat_age), dog(dog_age)]
