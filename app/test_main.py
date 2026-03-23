8c50ff888fc850fa74baa9b72b5e42d0f7db225d
Denys Baliuk <denuk14228@gmail.com>
2026-03-23 22:40:44 +0200

    cat and dog years 2

--- app/test_main.py
+++ app/test_main.py
@@ -3,37 +3,35 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age,dog_age,expected_result",
+    "cat_age, dog_age, expected",
     [
         (0, 0, [0, 0]),
+        (1, 1, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
+        (16, 16, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
+        (30, 35, [3, 3]),
+        (40, 50, [6, 8]),
         (100, 100, [21, 17]),
-    ]
+    ],
 )
-def test_get_human_age_valid_cases(
-    cat_age: int,
-    dog_age: int,
-    expected_result: list
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected_result
+def test_get_human_age(cat_age, dog_age, expected):
+    assert get_human_age(cat_age, dog_age) == expected
 
 
-@pytest.mark.parametrize(
-    "cat_age,dog_age",
-    [
-        (-1, 5),
-        (5, -1),
-        ("15", 15),
-        (15, "15"),
-        (None, 10),
-        (10, []),
-    ]
-)
-def test_get_human_age_invalid_inputs(cat_age: any, dog_age: any) -> None:
-    with pytest.raises((ValueError, TypeError)):
-        get_human_age(cat_age, dog_age)
+def test_cat_edge_cases():
+    assert get_human_age(14, 0)[0] == 0
+    assert get_human_age(15, 0)[0] == 1
+    assert get_human_age(24, 0)[0] == 2
+    assert get_human_age(28, 0)[0] == 3
+
+
+def test_dog_edge_cases():
+    assert get_human_age(0, 14)[1] == 0
+    assert get_human_age(0, 15)[1] == 1
+    assert get_human_age(0, 24)[1] == 2
+    assert get_human_age(0, 29)[1] == 3
