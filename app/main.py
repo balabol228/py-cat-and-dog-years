7f4e2f14a1789f4c09bf00bdc46857fc6b26bdb7
Denys Baliuk <denuk14228@gmail.com>
2026-03-23 23:17:21 +0200

    cat and dog years 16

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 def get_human_age(cat_age: int, dog_age: int) -> list[int]:
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise TypeError
+    if cat_age < 0 or dog_age < 0:
+        raise ValueError
+
     def calculate(age: int, step: int) -> int:
         if age < 15:
             return 0
